fa75aa913c17944b8c1b193c373e9e531f1ab7f0
Ryan Wells <ryanwells.com@gmail.com>
2021-01-03 13:27:44 -0500

    reduced size

--- src/components/GithubCorner.vue
+++ src/components/GithubCorner.vue
@@ -6,7 +6,7 @@
       target="_blank"
       rel="noopener"
       aria-label="View source on GitHub"
-      ><svg width="80" height="80" viewBox="0 0 250 250" class="colors">
+      ><svg viewBox="0 0 250 250" class="colors">
         <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
         <path
           d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
@@ -42,6 +42,8 @@ export default {
 }
 
 svg {
+  width: 50px;
+  height: 50px;
   fill: #000;
   color: #fff;
   opacity: 0.2;
