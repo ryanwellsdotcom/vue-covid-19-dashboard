ee4da5a451545aac061a7a0a8aed77306e90bb23
Ryan Wells <ryanwells.com@gmail.com>
2021-01-03 19:50:33 -0500

    moved footer comp from home to app

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,24 @@
     <GithubCorner />
     <Home />
   </div>
+  <footer>
+    <div class="wrapper">
+      <Footer />
+    </div>
+  </footer>
 </template>
 
 <script>
 import Home from './router/Home';
 import GithubCorner from './components/GithubCorner';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
   components: {
     Home,
-    GithubCorner
+    GithubCorner,
+    Footer
   }
 };
 </script>
@@ -23,13 +30,28 @@ export default {
 
 body {
   font-family: 'Roboto', sans-serif;
-  background: #eee;
+  background: #fff;
   margin: 0;
 }
 
+#app {
+  position: absolute;
+
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-flow: column;
+}
 .wrapper {
+  width: calc(100% - 30px);
   max-width: 960px;
   margin: 0 auto;
   padding: 0 15px;
+}
+
+footer {
+  flex-grow: 1;
+  padding: 30px 0;
+  background-color: #eee;
 }
 </style>
